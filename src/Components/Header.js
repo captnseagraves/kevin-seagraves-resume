2611c3494a4bfd29d4bd4907d67fe46426a53797
Kevin Seagraves <k.s.seagraves@gmail.com>
2020-07-12 00:04:59 -0700

    Init. Image and bio updates. Remove miscellaneous items.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ParticlesBg  from "particles-bg";
 
 class Header extends Component {
   render() {
@@ -17,7 +16,6 @@ class Header extends Component {
 
     return (
       <header id="home">
-      <ParticlesBg type="circle" bg={true} />
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
@@ -32,13 +30,12 @@ class Header extends Component {
       </nav>
 
       <div className="row banner">
-      
+
          <div className="banner-text">
             <h1 className="responsive-headline">{name}</h1>
-            <h3>{description}.</h3>
+            <h3 style={{color: "white"}}>{description}</h3>
             <hr />
             <ul className="social">
-               <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
                <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
             </ul>
          </div>
